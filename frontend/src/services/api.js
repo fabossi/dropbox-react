307a6c7da8973aa0271bdeefb788e79e870589d8
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-02-24 22:44:26 -0300

    [FABOSSI] Only cors erros

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://dropbox-backend-fabossi.herokuapp.com'
+    // baseURL: 'https://dropbox-backend-fabossi.herokuapp.com'
+    baseURL: 'http://localhost:3000'
 });
 
 export default api;
